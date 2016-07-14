66ced2bf021d80239a2cb7ffe996d5ff369963e2
HuuLuong <huuluong@techmasters-Mac-mini-2.local>
2016-07-14 11:31:34 +0700

    first commit

--- DemoPopover/ViewController.swift
+++ DemoPopover/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
 
+    @IBOutlet weak var textView_comment: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textView_comment.layer.borderWidth = 2
+        textView_comment.layer.borderColor = UIColor.grayColor().CGColor
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showView" {
+            let controller = segue.destinationViewController
+            controller.popoverPresentationController?.delegate = self
+            controller.preferredContentSize = CGSize(width: 222, height: 30)
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .None
     }
 
-
+    //Bắt sự kiện khi tap các button trên popover view
+    //Sử dụng thuộc tính Tag để phân biệt giữa các nút
+    
+    
+    
 }
 
